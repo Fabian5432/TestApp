7b484a52d40fc393002170bd6a8ee8353fbd67dc
Fabian5432 <vlasceanufabian96@gmail.com>
2021-01-24 23:55:31 +0200

    Added integration tests

---
+++ IntegrationTests/CurrencyLayerServiceIntegrationTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Services;
+
+namespace IntegrationTests
+{   
+    [TestFixture]
+    class CurrencyLayerServiceIntegrationTests
+    {
+        [Test]
+        public async Task ShouldGetLiveValuesWithValidAccessKey()
+        {
+            // Arrange 
+            var SUT = new CurrencyLayerService("980b4fe950b3d8d23a5a6eef697865af");
+
+            // Act 
+            var result = await SUT.GetAllQuotes();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfAppIdIsInvalid()
+        {
+            // Arrange
+            var SUT = new OpenExchangeRatesService("dummy");
+
+            // Assert
+            Assert.ThrowsAsync<Exception>(async () => await SUT.GetAllRates());
+        }
+    }
+}
---
+++ IntegrationTests/OpenExchangeRatesServiceIntergrationTests.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using TestApp.Services;
+
+namespace IntegrationTests
+{   
+    [TestFixture]
+    public class OpenExchangeRatesServiceIntergrationTests
+    {
+        [Test]
+        public async Task ShouldGetLatestValuesWithValidAppId()
+        {  
+            // Arrange 
+            var SUT = new OpenExchangeRatesService("1de86dfd996b4c9da20c0b3fa6eefaa4");
+
+            // Act 
+            var result = await SUT.GetAllRates();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfAppIdIsInvalid()
+        {
+            // Arrange
+            var SUT = new OpenExchangeRatesService("dummy");
+
+            // Assert
+            Assert.ThrowsAsync<Exception>(async () => await SUT.GetAllRates());
+        }
+    }
+}
